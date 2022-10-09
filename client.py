864afb4337d613ff8738d695df4d36ce36c4a10f
Ritesh Hon <honritesh@gmail.com>
2022-10-09 19:43:24 +0530

    task 1 completed

--- client.py
+++ client.py
@@ -42,9 +42,10 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if (price_b == 0):
+    if price_b == 0:
         return
-    return price_a/price_b
+    else:
+        return price_a/price_b
 
 
 # Main
